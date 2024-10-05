f7dee7340b0ca72bd4cb603b07ae1f50e5b82b8d
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2024-10-05 12:06:05 +0530

    fix error cor

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const mongoose  = require('mongoose');
 const app= express();
-const cors = require('cors');
+const cors=require("cors");
 const userRouter = require('./routes/userRoute');
 require('dotenv').config()
 
 const port = process.env.NODE_PORT || 4000;
 
 
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
- 
-
-
 
 app.get('/',(req,res)=>{
     res.send("Hello gym app")
